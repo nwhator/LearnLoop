12ec1d838aeb50e319825dfda087ca068fe858cb
nwhator <nwhator@gmail.com>
2026-03-28 23:35:18 +0100

    feat: initialize Next.js configuration and PostCSS setup for Tailwind CSS integration

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Already has built-in support for Tailwind, but good to have the file
+    reactStrictMode: true,
+};
+
+export default nextConfig;
